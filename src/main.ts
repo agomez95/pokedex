b09e81f83f3429b64984d529e865a52526747948
agomez95 <agomez95@MacBook-Air-de-Adrian.local>
2023-01-08 11:55:54 -0500

    mongo files cleaned

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function main() {
   const app = await NestFactory.create(AppModule);
 
+  //prefijo global para el API: /api/pokemon
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
