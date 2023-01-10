f9090c078ade8a42a6a99e45d3e7532a27e6b966
Adrian Gomez V <sandmancity14@gmail.com>
2023-01-10 11:56:52 -0500

    use of ValidationSchema with joi

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -6,5 +6,5 @@ export const EnvConfiguration = () => ({
     enviroment: process.env.NODE_ENV || 'dev',
     mongodb: process.env.MONGODB,
     port: process.env.PORT,
-    defaultLimit: process.env.DEFAULT_LIMIT || 5,
+    defaultLimit: +process.env.DEFAULT_LIMIT,
 })
